113ca9eb380f572cffb9b524f7440f9dda07f5b6
victor loginov <tkoe88@zoho.com>
2023-12-29 22:26:42 +0100

    added guard clause

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,8 @@ function App() {
     duration: 10,
   });
 
+  const valid = userInput.duration > 0;
+
   function handleChange(inputId, newValue) {
     setUserInput((prevData) => {
       return { ...prevData, [inputId]: +newValue }; // adding + operator infront of value insures it is converted from string to integer
@@ -19,10 +21,10 @@ function App() {
 
   return (
     <>
-    <Header />
-    <UserInput userInput={userInput} handleChange={handleChange} />
-    <Results userInput={userInput} />
-  </>
+      <Header />
+      <UserInput userInput={userInput} handleChange={handleChange} />
+      {valid ? <Results userInput={userInput} /> : <p className="center">Enter duration</p>}
+    </>
   );
 }
 
